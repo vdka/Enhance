85188f3731c56b1435ec93543dea3180dabb3282
Ethan Jackwitz <ethanjackwitz@gmail.com>
2023-07-11 21:04:32 +1000

    feat: Backport iOS17 SwiftUI defaultFocus as prefersDefaultFocus overload

---
+++ Sources/Enhance/SwiftUI+Extensions/Focus+Extensions.swift
@@ -0,0 +1,20 @@
+
+import SwiftUI
+
+public extension View {
+
+    @ViewBuilder
+    @_disfavoredOverload
+    @available(iOS, introduced: 16)
+    func prefersDefaultFocus<V>(
+        _ binding: FocusState<V>.Binding,
+        _ value: V,
+        priority: DefaultFocusEvaluationPriority = .automatic
+    ) -> some View where V : Hashable {
+        if #available(iOS 17, macOS 13, tvOS 16, watchOS 9, *) {
+            self.defaultFocus(binding, value, priority: priority)
+        } else {
+            self.onAppear(perform: { binding.wrappedValue = value })
+        }
+    }
+}
