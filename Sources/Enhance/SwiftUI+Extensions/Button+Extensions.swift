746f01bf03391292ed603a0b16f3df01a195d51d
Ethan Jackwitz <ethanjackwitz@gmail.com>
2023-07-31 10:00:19 +1000

    fix: Conditionally compile Button initializers with systemImage parameters as they were added to SwiftUI in Xcode15B5

--- Sources/Enhance/SwiftUI+Extensions/Button+Extensions.swift
+++ Sources/Enhance/SwiftUI+Extensions/Button+Extensions.swift
@@ -1,6 +1,9 @@
 
 import SwiftUI
 
+/// - Note: Introduced to SwiftUI in Xcode15B5
+/// - SeeAlso: https://developer.apple.com/documentation/swiftui/button/init(_:systemimage:action:)-6346x
+#if swift(<5.9)
 public extension Button where Label == SwiftUI.Label<Text, Image> {
     init(_ label: LocalizedStringKey, systemImage: String, role: ButtonRole? = nil, action: @escaping () -> Void) {
         self.init(role: role, action: action) {
@@ -15,6 +18,7 @@ public extension Button where Label == SwiftUI.Label<Text, Image> {
         }
     }
 }
+#endif
 
 public extension Button where Label == Text {
 
