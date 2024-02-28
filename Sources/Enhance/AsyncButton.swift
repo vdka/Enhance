2706929d5ea220a8a535f868bdbcb4b0161dac35
Ethan Jackwitz <ethanjackwitz@gmail.com>
2024-02-28 13:56:36 +1000

    feat: Add an optional binding to set an AsyncButton as isLoading without needing the button to be triggered

--- Sources/Enhance/AsyncButton.swift
+++ Sources/Enhance/AsyncButton.swift
@@ -1,87 +1,114 @@
 
 import SwiftUI
 
-// See: https://www.swiftbysundell.com/articles/building-an-async-swiftui-button
+/*
+ * See https://www.swiftbysundell.com/articles/building-an-async-swiftui-button
+ * See https://www.fivestars.blog/articles/optional-binding/
+ */
 
 public struct AsyncButton<Label: View>: View {
     public var options: AsyncButtonOption = AsyncButtonOption.all
     public var role: ButtonRole? = nil
     public var errorTitle: String?
+
+    @State var isLoadingStored: Bool = false
+    var isLoading: Binding<Bool>?
+
     public var action: () async throws -> Void
     @ViewBuilder public var label: () -> Label
 
-    @State public var isDisabled = false
-    @State public var showProgressView = false
     @State public var error: Error?
 
-    @Environment(\.handleError) var handleError
-
     public init(
-        options: AsyncButtonOption = .all,
+        options: AsyncButtonOption = AsyncButtonOption.all,
         role: ButtonRole? = nil,
         errorTitle: String? = nil,
+        isLoading: Binding<Bool>? = nil,
         action: @escaping () async throws -> Void,
-        @ViewBuilder label: @escaping () -> Label
+        label: @escaping () -> Label
     ) {
         self.options = options
         self.role = role
         self.errorTitle = errorTitle
+        self.isLoading = isLoading
         self.action = action
         self.label = label
     }
 
     public var body: some View {
-        Button(role: role) {
-            if options.contains(.disableButton) {
-                isDisabled = true
-            }
-
-            Task {
-                var progressViewTask: Task<Void, Error>?
-
-                if options.contains(.showProgressView) {
-                    progressViewTask = Task {
-                        try await Task.sleepThrowingOnCancellation(seconds: 0.15) // Display progress view only after 0.15 seconds
-                        showProgressView = true
-                    }
-                }
-
-                do {
-                    try await action()
-                } catch {
-                    self.error = error
-                    handleError(title: errorTitle, error)
-                    await Task.sleep(seconds: 0.5)
-                    self.error = nil
-                }
-                progressViewTask?.cancel()
-
-                isDisabled = false
-                showProgressView = false
-            }
-        } label: {
-            if error != nil {
-                Image(systemName: "exclamationmark.circle.fill").foregroundStyle(.red)
-                    .transition(.move(edge: .bottom).combined(with: .opacity))
-            } else if showProgressView {
-                ZStack {
-                    ProgressView()
-                    label().opacity(0)
-                }
-                .transition(.move(edge: .bottom).combined(with: .opacity))
-            } else {
-                label().transition(.move(edge: .top).combined(with: .opacity))
-            }
-        }
-        .animation(.easeInOutBack, value: showProgressView)
-        .animation(.easeInOutBack, value: error != nil)
-        .disabled(isDisabled || error != nil)
+        _AsyncButton(
+            options: options,
+            role: role,
+            isLoading: isLoading ?? $isLoadingStored,
+            errorTitle: errorTitle,
+            action: action,
+            label: label
+        )
     }
 
     func withErrorTitle(_ title: String) -> Self {
         var copy = self
         copy.errorTitle = title
         return copy
+    }
+
+    struct _AsyncButton: View {
+        var options: AsyncButtonOption = AsyncButtonOption.all
+        var role: ButtonRole? = nil
+        @Binding var isLoading: Bool
+        var errorTitle: String?
+        var action: () async throws -> Void
+        @ViewBuilder var label: () -> Label
+
+        @State var error: Error?
+
+        var showProgressView: Bool { isLoading && options.contains(.showProgressView) }
+        var isDisabled: Bool { isLoading && options.contains(.disableButton) }
+
+        @Environment(\.handleError) var handleError
+
+        var body: some View {
+            Button(role: role) {
+                isLoading = true
+
+                Task {
+                    defer { isLoading = false }
+                    var progressViewTask: Task<Void, Error>?
+
+                    if options.contains(.showProgressView) {
+                        progressViewTask = Task {
+                            try await Task.sleepThrowingOnCancellation(seconds: 0.15) // Display progress view only after 0.15 seconds
+                        }
+                    }
+
+                    do {
+                        try await action()
+                    } catch {
+                        self.error = error
+                        handleError(title: errorTitle, error)
+                        await Task.sleep(seconds: 0.5)
+                        self.error = nil
+                    }
+                    progressViewTask?.cancel()
+                }
+            } label: {
+                if error != nil {
+                    Image(systemName: "exclamationmark.circle.fill").foregroundStyle(.red)
+                        .transition(.move(edge: .bottom).combined(with: .opacity))
+                } else if showProgressView {
+                    ZStack {
+                        ProgressView()
+                        label().opacity(0)
+                    }
+                    .transition(.move(edge: .bottom).combined(with: .opacity))
+                } else {
+                    label().transition(.move(edge: .top).combined(with: .opacity))
+                }
+            }
+            .animation(.easeInOutBack, value: showProgressView)
+            .animation(.easeInOutBack, value: error != nil)
+            .disabled(isDisabled || error != nil)
+        }
     }
 }
 
@@ -100,10 +127,11 @@ public extension AsyncButton where Label == Text {
     init(
         _ title: LocalizedStringKey,
         role: ButtonRole? = nil,
+        isLoading: Binding<Bool>? = nil,
         options: AsyncButtonOption = .all,
         action: @escaping () async throws -> Void
     ) {
-        self.init(options: options, role: role, action: action) {
+        self.init(options: options, role: role, isLoading: isLoading, action: action) {
             Text(title)
         }
         if let key = title.key, key.contains("%") == false {
@@ -115,10 +143,11 @@ public extension AsyncButton where Label == Text {
     init(
         _ title: some StringProtocol,
         role: ButtonRole? = nil,
+        isLoading: Binding<Bool>? = nil,
         options: AsyncButtonOption = .all,
         action: @escaping () async throws -> Void
     ) {
-        self.init(options: options, role: role, action: action) {
+        self.init(options: options, role: role, isLoading: isLoading, action: action) {
             Text(title)
         }
         self.errorTitle = String(localized: "\(String(title)) Failed")
@@ -129,10 +158,11 @@ public extension AsyncButton where Label == Image {
     init(
         systemImage: String,
         role: ButtonRole? = nil,
+        isLoading: Binding<Bool>? = nil,
         options: AsyncButtonOption = .all,
         action: @escaping () async throws -> Void
     ) {
-        self.init(options: options, role: role, action: action) {
+        self.init(options: options, role: role, isLoading: isLoading, action: action) {
             Image(systemName: systemImage)
         }
     }
@@ -143,10 +173,11 @@ public extension AsyncButton where Label == SwiftUI.Label<Text, Image> {
         _ title: LocalizedStringKey,
         systemImage: String,
         role: ButtonRole? = nil,
+        isLoading: Binding<Bool>? = nil,
         options: AsyncButtonOption = .all,
         action: @escaping () async throws -> Void
     ) {
-        self.init(options: options, role: role, action: action) {
+        self.init(options: options, role: role, isLoading: isLoading, action: action) {
             SwiftUI.Label(title, systemImage: systemImage)
         }
         if let key = title.key, key.contains("%") == false {
@@ -159,10 +190,11 @@ public extension AsyncButton where Label == SwiftUI.Label<Text, Image> {
         _ title: some StringProtocol,
         systemImage: String,
         role: ButtonRole? = nil,
+        isLoading: Binding<Bool>? = nil,
         options: AsyncButtonOption = .all,
         action: @escaping () async throws -> Void
     ) {
-        self.init(options: options, role: role, action: action) {
+        self.init(options: options, role: role, isLoading: isLoading, action: action) {
             SwiftUI.Label(title, systemImage: systemImage)
         }
         self.errorTitle = String(localized: "\(String(title)) Failed")
@@ -172,10 +204,11 @@ public extension AsyncButton where Label == SwiftUI.Label<Text, Image> {
         verbatim title: some StringProtocol,
         systemImage: String,
         role: ButtonRole? = nil,
+        isLoading: Binding<Bool>? = nil,
         options: AsyncButtonOption = .all,
         action: @escaping () async throws -> Void
     ) {
-        self.init(options: options, role: role, action: action) {
+        self.init(options: options, role: role, isLoading: isLoading, action: action) {
             SwiftUI.Label(verbatim: title, systemImage: systemImage)
         }
         self.errorTitle = String(localized: "\(String(title)) Failed")
@@ -186,28 +219,48 @@ public extension AsyncButton where Label == Text {
     init(
         verbatim title: some StringProtocol,
         role: ButtonRole? = nil,
+        isLoading: Binding<Bool>? = nil,
         options: AsyncButtonOption = .all,
         action: @escaping () async throws -> Void
     ) {
-        self.init(options: options, role: role, action: action) {
+        self.init(options: options, role: role, isLoading: isLoading, action: action) {
             Text(title)
         }
         self.errorTitle = String(localized: "\(String(title)) Failed")
     }
 }
 
-struct AsyncButton_Previews: PreviewProvider {
-    static var previews: some View {
+#Preview {
+    return Group {
         AsyncButton("Cancel", action: {
             await Task.sleep(seconds: 1)
-            throw Error.error
+            throw URLError(.badServerResponse)
         })
-        .buttonStyle(.bordered)
         .handleError(using: { print($0 ?? "nil", $1) })
+        SampleIsLoadingBinding()
+    }
+    .buttonStyle(.bordered)
+}
+
+fileprivate struct SampleIsLoadingBinding: View {
+    @State var isLoading = false
+
+    var body: some View {
+        Button("Trigger", action: action)
+
+        AsyncButton("Async Button", isLoading: $isLoading, action: { await Task.sleep(seconds: 1) })
     }
 
-    private enum Error: Swift.Error {
-        case error
+    func action() {
+        isLoading = true
+        Task {
+            defer { isLoading = false }
+            await actionBody()
+        }
+    }
+
+    func actionBody() async {
+            await Task.sleep(seconds: 1)
     }
 }
 
